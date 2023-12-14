6cb6d1a047622185532f607ad3c03ba20cce991b
AnufrikovaViktoria <Phoenix-Viki@yandex.ru>
2023-12-14 09:08:38 +0700

    My first commit

---
+++ configuration.py
@@ -0,0 +1,10 @@
+URL_SERVICE = "https://603eebf6-c897-475a-8ce6-20638f5d5278.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/" #логи основного сервера
+
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv" #получение информации из таблицы
+
+CREATE_USER_PATH = "/api/v1/users/" #создание пользователя
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/" #поиск наборов по продуктам
